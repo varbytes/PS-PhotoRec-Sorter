12ce1f75a71f011fbbe0a7c5e4b6db4c06f732ed
varbytes <51965307+varbytes@users.noreply.github.com>
2025-05-06 13:34:39 +0800

    Aligned spaces and tabs

--- PS-PhotoRec-Sorter.ps1
+++ PS-PhotoRec-Sorter.ps1
@@ -1,7 +1,7 @@
 ﻿<#
 .SYNOPSIS
     This script will sort files recovered by Photorec into folders by file extension based on Photorec's 'report.xml' and 
-	generate an Excel report.
+    generate an Excel report.
 .DESCRIPTION
     An Excel version of Photorec's report.xml will be created and recovered files will be sorted into folders by extension.
     Files which cannot be located or not listed in report.xml will be left as-is.
@@ -11,7 +11,7 @@
     Sorts Photorec recovered files into folders by file extension and generates Excel version of report.xml.
 .NOTES
     Author: Y.G.
-	GitHub: @varbytes
+    GitHub: @varbytes
     Date:   6 May 2025
 #>
 
@@ -44,7 +44,7 @@ Try {
     # Setup header row
     $excelWorksheet.Cells.Item(1, 1) = "S/N"
     $excelWorksheet.Cells.Item(1, 2) = "Filename"
-	$excelWorksheet.Cells.Item(1, 3) = "PhotoRec Filename"
+    $excelWorksheet.Cells.Item(1, 3) = "PhotoRec Filename"
     $excelWorksheet.Cells.Item(1, 4) = "File Ext"
     $excelWorksheet.Cells.Item(1, 5) = "File Size"
     $excelWorksheet.Cells.Item(1, 6) = "MD5 Hash"
@@ -55,38 +55,38 @@ Try {
     foreach ($entry in $xmlFile.dfxml.fileobject) {
         # Locate carved file
         $filepath = $entry.filename
-		if ($filepath.contains("recup_dir")) {
+        if ($filepath.contains("recup_dir")) {
             $filepath = $filepath.split("/",3)[2]
         } else {
             $filepath = "recup_dir.1/" + $filepath
         }
         if (!(Test-Path $filepath)) {
             Write-Host "[ERROR] Could not find file '${filepath}'. Trying closest match..."
-			$tempname = $filepath.Substring(0, $filepath.lastIndexOf("."))
-			$tempext = $filepath.split(".")[-1]
-			$filepath = -join($tempname, "_*.", $tempext)
-			if (!(Test-Path $filepath)) {
-				Write-Host "[ERROR] No match found for '${filepath}'."
-				Continue
-			}
+            $tempname = $filepath.Substring(0, $filepath.lastIndexOf("."))
+            $tempext = $filepath.split(".")[-1]
+            $filepath = -join($tempname, "_*.", $tempext)
+            if (!(Test-Path $filepath)) {
+                Write-Host "[ERROR] No match found for '${filepath}'."
+                Continue
+            }
         }
 
         # Get carved file and move file into folder
         $file = Get-Item -Path $filepath
-		$fileExt = $file.Extension
-		Write-Host "[INFO] Hashing '$($file.Name)'."
+        $fileExt = $file.Extension
+        
+        Write-Host "[INFO] Hashing '$($file.Name)'."
         $fileHash = (Get-FileHash -Path $file.FullName -Algorithm MD5).Hash.ToLower()
         $dstPath = Join-Path -Path $curPath -ChildPath $fileExt.ToUpper()
-        if (!(Test-Path $dstPath -PathType Container))
-	    {
-		    New-Item -ItemType Directory -Path $dstPath
-	    }
+        if (!(Test-Path $dstPath -PathType Container)) {
+            New-Item -ItemType Directory -Path $dstPath
+        }
         Move-Item -Path $file.FullName -Destination $dstPath
 
         # Print details of carved file to Excel sheet
         $excelWorksheet.Cells.Item($x, 1) = $x - 1
         $excelWorksheet.Cells.Item($x, 2) = $file.Name
-		$excelWorksheet.Cells.Item($x, 3) = $entry.filename
+        $excelWorksheet.Cells.Item($x, 3) = $entry.filename
         $excelWorksheet.Cells.Item($x, 4) = $fileExt
         $excelWorksheet.Cells.Item($x, 5) = $entry.filesize
         $excelWorksheet.Cells.Item($x, 6) = $fileHash
@@ -106,7 +106,7 @@ Try {
     $range.Font.Bold = $True
     $excelWorksheet.Cells.Item(1,1).ColumnWidth = 8
     $excelWorksheet.Cells.Item(1,2).ColumnWidth = 40
-	$excelWorksheet.Cells.Item(1,3).ColumnWidth = 40
+    $excelWorksheet.Cells.Item(1,3).ColumnWidth = 40
     $excelWorksheet.Cells.Item(1,4).ColumnWidth = 10
     $excelWorksheet.Cells.Item(1,5).ColumnWidth = 15
     $excelWorksheet.Cells.Item(1,6).ColumnWidth = 40
@@ -129,14 +129,12 @@ Try {
     Write-Host "'photorec-results.xlsx' and 'report.xml' can be found in '$curPath'"
 
 }
-Catch
-{
+Catch {
     Write-Host("TRAPPED: " + $_.ItemName)
     Write-Host("[ERROR!] " + $_.Exception.Message)
     continue  # continue onto the "Finally" block
 }
-Finally
-{
+Finally {
     # Close the COM object
-	$excelFile.Quit()
+    $excelFile.Quit()
 }
